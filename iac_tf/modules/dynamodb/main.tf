f84b7685237617c04d5fbe27860403689941f1eb
Akash Suvarna <akash.suvarna@live.com>
2024-10-23 22:44:14 +0530

    [UPD] Resource name for DynamoDB tbl for shortener

--- iac_tf/modules/dynamodb/main.tf
+++ iac_tf/modules/dynamodb/main.tf
@@ -1,4 +1,4 @@
-resource "aws_dynamodb_table" "basic-dynamodb-table" {
+resource "aws_dynamodb_table" "url-shortener-table" {
   name           = "url-shortener"
   billing_mode   = "PROVISIONED"
   read_capacity  = 10
